92c0d64a83262be968c4fb58f60d4430c6ccb5fa
George Hamann <george.hamann@gmail.com>
2024-08-01 20:17:58 -0300

    Mvc refatorado para o crud.

--- src/ControleDeBar/Controllers/ClientesController.cs
+++ src/ControleDeBar/Controllers/ClientesController.cs
@@ -29,8 +29,7 @@ namespace ControleDeBar
 
         public IActionResult Index()
         {
-            ViewBag.Clientes = Clientes;
-            return View();
+            return View(Clientes);
         }
 
         public IActionResult Detalhes(int id = 0)
@@ -40,22 +39,95 @@ namespace ControleDeBar
             return View();
         }
 
-        [HttpPost]
+        [HttpGet]
         public IActionResult Adicionar()
         {
-            return View();
+            ViewBag.Error = string.Empty;
+            return View(new Cliente());
         }
 
         [HttpPost]
-        public IActionResult Editar()
+        public IActionResult Adicionar(Cliente cliente)
         {
-            return View();
+            bool existeCliente = false;
+
+            existeCliente = Clientes.Exists(clienteLista => clienteLista.Nome.Equals(cliente.Nome));
+
+            if (existeCliente)
+            {
+                ViewBag.Error = "Já Existe esse cliente";
+            }
+            else
+            {
+                ViewBag.Error = string.Empty;
+                Clientes.Add(cliente);
+            }
+
+            return View(cliente);
+        }
+
+
+        public IActionResult Editar(int id = 0)
+        {
+            Cliente cliente = Clientes.FirstOrDefault(cliente => cliente.Id == id.ToString());
+            ViewBag.Index = Clientes.LastIndexOf(cliente);
+            ViewBag.Error = string.Empty;
+            return View(cliente);
         }
 
         [HttpPost]
-        public IActionResult Deletar()
+        public IActionResult Editar(Cliente cliente, int index)
         {
-            return View();
+            bool existeCliente = false;
+
+            existeCliente = Clientes.Exists(clienteLista => clienteLista.Nome.Equals(cliente.Nome));
+            ViewBag.Index = index;
+            if (existeCliente)
+            {
+                ViewBag.Error = "Já existe cliente com esse nome";
+            }
+            else
+            {
+                Clientes[index] = cliente;
+            }
+            return View(cliente);
+        }
+
+
+        public IActionResult Deletar(int id = 0)
+        {
+            Cliente cliente = Clientes.FirstOrDefault(cliente => cliente.Id == id.ToString());
+            ViewBag.Index = Clientes.LastIndexOf(cliente);
+            ViewBag.Error = string.Empty;
+            ViewBag.DeleteSucesso = false;
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Cliente cliente, int index)
+        {
+            ViewBag.Index = index;
+            ViewBag.DeleteSucesso = false;
+            try
+            {
+                Cliente confirmarCliente = Clientes[index];
+                if (confirmarCliente.Id == cliente.Id)
+                {
+
+                    Clientes.RemoveAt(index);
+                    ViewBag.DeleteSucesso = true;
+                }
+                else
+                {
+                    ViewBag.Error = "Cliente não confirmado, revise os dados.";
+                }
+            }
+            catch
+            {
+                ViewBag.Error = "Cliente não confirmado, revise os dados.";
+            }
+
+            return View(cliente);
         }
 
 
--- src/ControleDeBar/Model/Cliente.cs
+++ src/ControleDeBar/Model/Cliente.cs
@@ -1,6 +1,10 @@
 ﻿namespace ControleDeBar.Model;
 public class Cliente
 {
+    public Cliente()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
     public string Id { get; set; }
     public string Nome { get; set; }
 
