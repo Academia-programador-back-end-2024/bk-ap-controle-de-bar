73ae619e998218cfc9b49f1bd7a3e821904b0ac7
George Hamann <george.hamann@gmail.com>
2024-08-06 20:11:27 -0300

    Inicio data annotation Auxiliares de marca

--- src/ControleDeBar/Model/Cliente.cs
+++ src/ControleDeBar/Model/Cliente.cs
@@ -1,4 +1,6 @@
-﻿namespace ControleDeBar.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeBar.Model;
 public class Cliente
 {
     public Cliente()
@@ -6,6 +8,9 @@ public class Cliente
         Id = Guid.NewGuid().ToString();
     }
     public string Id { get; set; }
+
+    [Required]
+    [Display(Description = "Nome cliente")]
     public string Nome { get; set; }
 
 }
