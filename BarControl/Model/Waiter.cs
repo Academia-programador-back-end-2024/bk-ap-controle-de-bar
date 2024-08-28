99164b8271cde17797ffc9293ffd3c1c4f27286e
TiagOzFiori <tiagozfzfiori@gmail.com>
2024-08-28 17:15:16 -0300

    Started fixing the waiter crud, some stuff in the frontend is not entirely correct but a good enough jump start

--- BarControl/Model/Waiter.cs
+++ BarControl/Model/Waiter.cs
@@ -7,12 +7,17 @@ public class Waiter : BaseModel
 {
     public Waiter() : base()
     {
-        
+        Id = Guid.NewGuid().ToString();
+        Age = 18;
     }
     
     [Required]
-    [Display(Description = "Client name")]
+    [Display(Description = "Waiter name")]
     [MinLength(length: 2, ErrorMessage ="Name needs to be at least 2 characters long")]
     [MaxLength(length: 20, ErrorMessage = "Name must not exceed 20 characters!")]
     public string Name { get; set; }
+    
+    [Required]
+    [Display(Name= "Age", Description = "Waiter's age")]
+    public int Age { get; set; }
 }
