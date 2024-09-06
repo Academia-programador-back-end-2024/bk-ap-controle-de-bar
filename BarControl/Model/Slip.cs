92b51ddc3d2ed53c4b2c9149b2b484c8b22087ed
TiagOzFiori <tiagozfzfiori@gmail.com>
2024-09-06 03:10:48 -0300

    Refactoring

--- BarControl/Model/Slip.cs
+++ BarControl/Model/Slip.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BarControl.Model;
 
 public class Slip : BaseModel
@@ -8,17 +10,22 @@ public class Slip : BaseModel
     }
 
     public string ClientId { get; set; }
+    [Display(Name = "Client name")]
     public virtual Client? Client { get; set; }
     
     public string TableId { get; set; }
+    [Display(Name = "Table number")]
     public virtual Table? Table { get; set; }
     
     public string WaiterId { get; set; }
+    [Display(Name = "Waiter name")]
     public virtual Waiter? Waiter { get; set; }
     
     public virtual  List<Consumption>? Consumptions { get; set; }
     
+    [Display(Name = "Opening date")]
     public DateTime OpeningDate { get; set; }
+    [Display(Name = "Closing date")]
     public DateTime? ClosingDate { get; set; }
     
     public bool Paid { get; set; } // Defaults to false
